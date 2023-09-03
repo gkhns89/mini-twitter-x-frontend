eb387e0f40dff23369cec7b3142cab0eeadca2cb
gkhns89 <gokhansisman@outlook.com>
2023-09-03 15:45:23 +0300

    Sign In designed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Landing from "./Components/Landing";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return (
     <div className="my-0 mx-auto ">
-      <Landing />
+      <SignUp />
     </div>
   );
 }
